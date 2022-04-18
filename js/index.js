620c4b1544cad96c40debf04f9078a7796a1d9c0
Dansf <dansf.3009@gmail.com>
2022-04-17 22:21:58 -0300

    Adding the carousel Glide CDN

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// GlideJs
+const glide = new Glide(".glide", {
+  type: "slider",
+  startAt: 0,
+  // perView: 2,
+  perTouch: 2,
+  gap: 5,
+  // focusAt: "center",
+  keyboard: false,
+  peek: {
+
+  },
+  // breakpoints: {
+  //   768: {
+
+  //   },
+  //   1024: {
+
+  //   },
+  // },
+}).mount()
